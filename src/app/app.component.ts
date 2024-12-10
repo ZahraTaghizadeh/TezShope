cb7bf1ba447e198123125b9e61459d07b707ebe7
z.taghizadeh <z.taghizadeh@TAGHIZADEHZ-PC>
2024-12-10 17:25:14 +0330

    creat add user componnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AddUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
